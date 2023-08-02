5501093dd4e49e80f0dba5b8c8796e0282c664c2
hanqi27 <126135339+hanqi27@users.noreply.github.com>
2023-08-02 15:31:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ false = null = true = ''
 def get_color():
     # 获取随机颜色
     get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
-    color_list =['#000000']
+    color_list =get_colors(100)
     return random.choice(color_list)
 
 
